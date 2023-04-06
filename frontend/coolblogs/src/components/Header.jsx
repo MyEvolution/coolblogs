b8161e24b4a85942f9f7a4638cf7aa36f536c644
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2023-04-06 15:49:57 -0400

    Header
    created components folder and added a header.jsx file to it

---
+++ frontend/coolblogs/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <p className="bg-black text-white p-5 text-2xl text-center font-bold font-mono">
+      Cool Blogs
+    </p>
+  );
+}
+
+export default Header;
