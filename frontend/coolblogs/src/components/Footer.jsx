fe75e1d094c61111bc7406158a41bc31d0408358
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2023-04-06 17:17:41 -0400

    Footer + some Styling & rendering
    Added a footer component,  added some basic styling in header, footer and blog posts. Posts now render using useEffect

---
+++ frontend/coolblogs/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <p className="bg-black text-white p-5 text-center font-mono fixed inset-x-0 bottom-0">
+      &copy; CoolBlogs Developed by Adam and Alaaeldean 2023
+    </p>
+  );
+}
+
+export default Footer;
